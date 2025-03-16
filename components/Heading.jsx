aee55f09cc0c453735d06e6e38859eed5b8bead5
Puspak29 <samantapuspak@gmail.com>
2025-03-16 10:42:00 +0530

    view venue added

---
+++ components/Heading.jsx
@@ -0,0 +1,11 @@
+function Heading({props}) {
+  return (
+    <section className="bg-white mb-5 shadow px-4 py-4">
+        <h1 className="text-2xl font-bold tracking-tight text-gray-900">
+          {props}
+        </h1>
+      </section>
+  )
+}
+
+export default Heading
--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Roomcard from "./Roomcard";
+import Heading from "./Heading";
+import BookingForm from "./BookingForm";
 
-export { Header, Footer, Roomcard };
+export { Header, Footer, Roomcard, Heading, BookingForm };
