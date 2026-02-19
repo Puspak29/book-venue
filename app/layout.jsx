c0fcca5533d2a2e4911628508374a341ce2a3933
Puspak29 <samantapuspak@gmail.com>
2026-02-19 17:13:35 +0530

    fix: nextjs version

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,17 +16,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <AuthWrapper>
     <html lang="en">
       <body className={ inter.className}>
+      <AuthWrapper>
         <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 md:px-7 lg:px-8">
           {children}
         </main>
         <Footer />
         <ToastContainer />
+      </AuthWrapper>
       </body>
     </html>
-    </AuthWrapper>
   );
 }
